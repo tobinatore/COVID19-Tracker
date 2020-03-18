59680a16f7089a88a079ac5e576d5d1046e60020
Tobias <germanicalp@gmail.com>
2020-03-18 19:43:58 +0100

    Commit #4 - Basic functionality
    views.py:
    - if the database is empty, the script queries the John Hopkins CSSE
    repository and fills the db with that data
    - index.html now gets the current number of infected, deaths and
    recovered patients worldwide, as well as the mortality rate
    
    api.py:
    - currently handles getting and formatting the data

--- covid19_tracker/tracker/api.py
+++ covid19_tracker/tracker/api.py
@@ -6,5 +6,84 @@ import os
 
 class API:
     
-    def __init__(self):
-        pass
+    def __init__(self):    
+        self.dirpath = os.getcwd()
+
+
+    def get_all(self):
+        """
+        Gets number of infections, deaths and recovered patients for every country.
+
+        Returns:
+            (Dict) A dict: {"country":"{"date":[cases, recoveries, deaths]}, ...}"
+        """
+        url_confirmed = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
+        url_deaths = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv"
+        url_recovered = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv"
+        
+        self.countries_dict = self.get_infected_countries(url_confirmed, url_deaths, url_recovered)
+        
+        return self.countries_dict
+
+    def get_infected_countries(self, url_confirmed, url_deaths, url_recovered):
+        """ Queries the Johns Hopkins CSSE's repository for all infected countries 
+            and builds a dict with confirmed cases, deaths and recoveries for every
+            country.
+
+            Arguments:
+            -- url_confirmed: (String) Url pointing to the .csv containing the numbers of confirmed cases
+            -- url_deaths: (String) Url pointing to the .csv containing the numbers of deaths
+            -- url_recovered: (String) Url pointing to the .csv containing the numbers of recovered cases
+
+            Returns: 
+                (Dict) A dict with the format {"country":"{"date":[cases, recoveries, deaths]}, ...}"
+               
+        """
+        data_dict = {}
+        prov_dict = {}
+
+        # Fetching the data from the Johns Hopkins CSSE's repository
+        conf = pd.read_csv(url_confirmed, error_bad_lines=False)
+        deaths = pd.read_csv(url_deaths, error_bad_lines=False)
+        recov = pd.read_csv(url_recovered, error_bad_lines=False)
+
+        # We do not need Latitude / Logitude information, so we'll just delete these columns
+        del conf['Lat'], conf['Long'], deaths['Lat'], deaths['Long'], recov['Lat'], recov['Long']
+        
+        # Making a list containing all dates in the .csv
+        cols = list(conf.columns.values)[2:]
+        inf = 0
+        d = 0
+        rec = 0 
+        yeet = 0
+        # Building the dict
+        for index, row in conf.iterrows():
+            data_deaths = deaths.iloc[index]
+            data_rec = recov.loc[index]
+            
+            if not row[1] in data_dict:
+                self.countries += 1
+                data_dict[row[1]] = {}
+                print(str(self.countries) + ": " + row[1])
+                for x in range(0, len(cols)):
+                    data_dict[row[1]][cols[x]] = [row[cols[x]], data_rec[cols[x]], data_deaths[cols[x]]]
+            elif row[0] in ['French Guiana', 'Guadeloupe', 'Guam', 'Mayotte', 'occupied Palestinian territory', 'Puerto Rico', 'Reunion']:
+                # the states in the list above are listed as territories in the .csv's, however they are countries at the same time,
+                # so we'll need to make an exception
+                self.countries += 1
+                data_dict[row[0]] = {}
+                for x in range(0, len(cols)):
+                    data_dict[row[0]][cols[x]] = [row[cols[x]], data_rec[cols[x]], data_deaths[cols[x]]]
+            else:
+                for x in range(0, len(cols)):
+                    data_dict[row[1]][cols[x]] = [x + y for x, y in zip(data_dict[row[1]][cols[x]], [row[cols[x]], data_rec[cols[x]], data_deaths[cols[x]]])]
+     
+        return data_dict
+
+            
+
+
+if __name__=="__main__":
+    # For debugging purposes you can run the api.py script ob its own
+    api = API()
+    api.get_all()
--- covid19_tracker/tracker/views.py
+++ covid19_tracker/tracker/views.py
@@ -1,3 +1,4 @@
+from django.template.defaultfilters import date as django_date_filter
 from django.http import HttpResponse
 from django.shortcuts import render
 from . import api
@@ -12,27 +13,52 @@ curr_dir = os.getcwd()
 
 def init_db(my_api):
     data = my_api.get_all()
+    c = 1
     for country in data:
+        print(str(c)+"/159", end='\r', flush=True)
         for date in data[country]:
             conf = data[country][date][0]
             deaths = data[country][date][2]
-            conf = data[country][date][1]
+            recov = data[country][date][1]            
+            date = datetime.datetime.strptime(date, '%m/%d/%y')
             d = Date(date=date, confirmed=conf, deaths=deaths, recovered=recov, country=country)
             d.save()
+        c+=1
 
 
 def update_db(my_api):
-    data = my_api.get_today()
+   # data = my_api.get_today()
+   pass
+
+
+def sum_cases():
+    data = Date.objects.filter(date=datetime.date.today())
+    if len(data) == 0:
+        data = Date.objects.filter(date=datetime.date.today() - datetime.timedelta(days = 1))
+
+    print(len(data))
+    inf = 0
+    deaths = 0
+    reco = 0
+    for date in data:
+        inf += date.confirmed
+        deaths += date.deaths
+        reco += date.recovered
     
+    return inf, deaths, reco
     
 
 def index(request):
+   
     my_api = api.API()
-    countries = my_api.get_countries()
 
-    if Date.objects.all() == []:
+    if Date.objects.count() == 0:
+        print("init")
         init_db(my_api)
     else:
         update_db(my_api)
 
-    return render(request,"index.html",{"infected":get_total_infected(os.path.join(curr_dir, "tracker", "json", "country_data.json")), "deaths":16000, "recovered":5000, "mortality_rate":5})
+    countries = len(set(Date.objects.all().values_list('country', flat=True)))
+    infected, deaths, recovered = sum_cases()
+    mortality = (deaths/infected)*100
+    return render(request,"index.html",{"infected":infected, "countries":countries, "deaths":deaths, "recovered":recovered, "mortality_rate":round(mortality,2)})
