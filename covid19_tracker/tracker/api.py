e748b4fae67a6db8cc5caa14ffde9803d74730d8
Tobias Nöthlich <germanicalp@gmail.com>
2020-03-20 15:37:40 +0100

    Commit #6 - Working graph
    Main page now shows trend of new infections, deaths and recoveries using
    a line graph.
    Added new methods to API class:
    - get_latest() - returns latest data from the JHU repository
    - fixed get_current_cases()
    views.py now updates database if it doesn't contain records for the last
    day

--- covid19_tracker/tracker/api.py
+++ covid19_tracker/tracker/api.py
@@ -14,11 +14,52 @@ class API:
         url="https://docs.google.com/spreadsheets/d/e/2PACX-1vR30F8lYP3jG7YOq8es0PBpJIE5yvRVZffOyaqC0GgMBN6yt0Q-NI8pxS7hd1F9dYXnowSC6zpZmW9D/pubhtml/sheet?headers=false&gid=0"
         req = requests.get(url)
         soup = BeautifulSoup(req.text, 'html.parser')
-        table = soup.findAll('td',class_="s4")
-        total = table[0].text.replace(",","")
-        deaths = table[1].text.replace(",","")
-        recov = soup.findAll('td',class_="s5")[0].text.replace(",","")
+        total = soup.findAll('td',class_="s42")[0].text.replace(",","")
+        deaths = soup.findAll('td',class_="s44")[0].text.replace(",","")
+        recov = soup.findAll('td',class_="s47")[0].text.replace(",","")
         return int(total), int(deaths), int(recov)
+
+    def get_latest(self):
+        url_confirmed = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
+        url_deaths = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv"
+        url_recovered = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv"
+
+        data_dict = {}
+        prov_dict = {}
+
+        # Fetching the data from the Johns Hopkins CSSE's repository
+        conf = pd.read_csv(url_confirmed, error_bad_lines=False)
+        deaths = pd.read_csv(url_deaths, error_bad_lines=False)
+        recov = pd.read_csv(url_recovered, error_bad_lines=False)
+
+        # We do not need Latitude / Logitude information, so we'll just delete these columns
+        del conf['Lat'], conf['Long'], deaths['Lat'], deaths['Long'], recov['Lat'], recov['Long']
+        
+        # Making a list containing the last date in the .csv
+        cols = list(conf.columns.values)[-1:]
+        inf = 0
+        d = 0
+        rec = 0 
+        print(cols)
+
+        # Building the dict
+        for index, row in conf.iterrows():
+            data_deaths = deaths.iloc[index]
+            data_rec = recov.loc[index]
+            
+            if not row[1] in data_dict:
+                data_dict[row[1]] = {}
+                data_dict[row[1]][cols[0]] = [row[cols[0]], data_rec[cols[0]], data_deaths[cols[0]]]
+            elif row[0] in ['French Guiana', 'Guadeloupe', 'Guam', 'Mayotte', 'occupied Palestinian territory', 'Puerto Rico', 'Reunion']:
+                # the states in the list above are listed as territories in the .csv's, however they are countries at the same time,
+                # so we'll need to make an exception
+                data_dict[row[0]] = {}
+                data_dict[row[0]][cols[0]] = [row[cols[0]], data_rec[cols[0]], data_deaths[cols[0]]]
+            else:
+                data_dict[row[1]][cols[0]] = [x + y for x, y in zip(data_dict[row[1]][cols[0]], [row[cols[0]], data_rec[cols[0]], data_deaths[cols[0]]])]
+     
+        return data_dict
+
 
 
     def get_all(self):
@@ -66,7 +107,7 @@ class API:
         inf = 0
         d = 0
         rec = 0 
-        yeet = 0
+
         # Building the dict
         for index, row in conf.iterrows():
             data_deaths = deaths.iloc[index]
--- covid19_tracker/tracker/views.py
+++ covid19_tracker/tracker/views.py
@@ -37,36 +37,79 @@ def init_db(my_api):
 
 
 def update_db(my_api):
-   # data = my_api.get_today()
-   pass
+    c = 1
+    data = my_api.get_latest()
+    for country in data:
+        print(str(c)+"/159", end='\r', flush=True)
+        for date in data[country]:
+            conf = data[country][date][0]
+            deaths = data[country][date][2]
+            recov = data[country][date][1]            
+            date = datetime.datetime.strptime(date, '%m/%d/%y')
+            d = Date(date=date, confirmed=conf, deaths=deaths, recovered=recov, country=country)
+            d.save()
+        c+=1
 
 
-def sum_cases(my_api):
-    #data = get_latest_data()
-#
-    #print(len(data))
-    #inf = 0
-    #deaths = 0
-    #reco = 0
-    #for date in data:
-        #inf += date.confirmed
-        #deaths += date.deaths
-        #reco += date.recovered
-#    
-    #return inf, deaths, reco
+def sum_cases():
+    dates = sorted(set(Date.objects.all().values_list('date', flat=True)))
+    conf_sum = []
+    death_sum = []
+    reco_sum = []
+    i = 2
+    for date in dates:
+        data = Date.objects.filter(date=date)
+        inf = 0
+        deaths = 0
+        reco = 0
+
+        for datapoint in data:
+            inf += datapoint.confirmed
+            deaths += datapoint.deaths
+            reco += datapoint.recovered    
+        
+        if i % 2 == 0:
+            conf_sum.append(inf)
+            death_sum.append(deaths)
+            reco_sum.append(reco)
+        i+=1
+
+    return dates[::2], conf_sum, death_sum, reco_sum
+
+
+def build_js(dates, conf_sum, death_sum, reco_sum):
+    string = "<script> "
+    string += "let dates = " + str(dates)+";"
+    string += "let conf_sum = " + str(conf_sum)+";"
+    string += "let death_sum = " + str(death_sum)+";"
+    string += "let reco_sum = " + str(reco_sum)+";"
+    string += "</script>"
+
+    print(string)
+
+    return string
+
+def curr_cases(my_api):
     return my_api.get_current_number()    
-
-def index(request):
    
+def index(request):
+    sum_cases()
     my_api = api.API()
 
     if Date.objects.count() == 0:
         print("init")
         init_db(my_api)
-    else:
+    elif len(Date.objects.filter(date=datetime.date.today() - datetime.timedelta(days = 1)).order_by("-confirmed")) == 0:
         update_db(my_api)
+    
 
+    ret = sum_cases()
+    dates, conf_sum, death_sum, reco_sum = ret[0], ret[1], ret[2], ret[3]
+    dates = [date.strftime('%d/%m/%y') for date in dates]
+    js = build_js(dates, conf_sum, death_sum, reco_sum)
     countries = len(set(Date.objects.all().values_list('country', flat=True)))
-    infected, deaths, recovered = sum_cases(my_api)
+    infected, deaths, recovered = curr_cases(my_api)
     mortality = (deaths/infected)*100
-    return render(request,"index.html",{"infected":infected, "countries":countries, "deaths":deaths, "recovered":recovered, "mortality_rate":round(mortality,2), "all":get_latest_data()})
+    return render(request,"index.html",{"infected":infected, "countries":countries, \
+        "deaths":deaths, "recovered":recovered, "mortality_rate":round(mortality,2), \
+        "all":get_latest_data(), "js":js})
