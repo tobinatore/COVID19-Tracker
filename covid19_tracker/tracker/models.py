aaf0f8abc4f0e1080803a0a93ce486be76e56367
Tobias Nöthlich <germanicalp@gmail.com>
2020-03-26 21:21:08 +0100

    Commit #11 - Small updates
    - changed fields for recovered cases, serious cases and active cases to
    CharField in Live model
    - fixed calculation of active cases on index.html
    - no longer shows global stats in table ( they are shown prominently on
    the site when no country is selected

--- covid19_tracker/tracker/models.py
+++ covid19_tracker/tracker/models.py
@@ -12,11 +12,15 @@ class Date(models.Model):
     def __str__(self):
         return str(self.date) + ": in: "+ self.country+": confirmed: "+ str(self.confirmed)+ ", deaths: "+ str(self.deaths)+", recovered: "+ str(self.recovered)
 
+class Live(models.Model):
     time = models.TimeField("time")
     confirmed = models.IntegerField("confirmed_cases")
     deaths = models.IntegerField("deaths")
+    recovered = models.CharField("recovered_cases",max_length=200)
     new_cases = models.IntegerField("new_cases")
     new_deaths = models.IntegerField("new_deaths")
+    serious = models.CharField("serious_cases",max_length=200)
     mortality = models.FloatField("mortality")
+    active = models.CharField("active_cases", max_length=200)
     country = models.CharField(max_length=200) 
 
